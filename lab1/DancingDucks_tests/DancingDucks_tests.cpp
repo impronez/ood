255e7f9710100c18a2cf5c7c587037ca340ebd0b
impronez <orzaev7312@gmail.com>
2024-09-10 12:08:02 +0300

    task 1

---
+++ lab1/DancingDucks_tests/DancingDucks_tests.cpp
@@ -0,0 +1,114 @@
+#define CATCH_CONFIG_MAIN
+#include "../../../../catch/catch.hpp"
+#include "../SimUDuck/DecoyDuck.h"
+#include "../SimUDuck/MallardDuck.h"
+#include "../SimUDuck/ModelDuck.h"
+#include "../SimUDuck/RedheadDuck.h"
+#include "../SimUDuck/RubberDuck.h"
+#include "../SimUDuck/DanceWaltz.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+
+const std::string DANCE_MINUET = "I'm dance a minuet!!\n";
+const std::string DANCE_WALTZ = "I'm dance a waltz!!\n";
+
+TEST_CASE("Test dance method of decoy duck")
+{
+	DecoyDuck decoyDuck;
+	std::ostringstream output;
+
+	std::streambuf* originalCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+	decoyDuck.Dance();
+
+	std::cout.rdbuf(originalCoutBuffer);
+
+	REQUIRE(output.str().empty());
+}
+
+TEST_CASE("Test dance method of mallard duck")
+{
+	MallardDuck mallardDuck;
+	std::ostringstream output;
+
+	std::streambuf* originalCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+	mallardDuck.Dance();
+
+	std::cout.rdbuf(originalCoutBuffer);
+
+	REQUIRE(output.str() == DANCE_WALTZ);
+}
+
+TEST_CASE("Test dance method of model duck")
+{
+	ModelDuck modelDuck;
+	std::ostringstream output;
+
+	std::streambuf* originalCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+	modelDuck.Dance();
+
+	std::cout.rdbuf(originalCoutBuffer);
+
+	REQUIRE(output.str().empty());
+}
+
+TEST_CASE("Test dance method of redhead duck")
+{
+	RedheadDuck redheadDuck;
+	std::ostringstream output;
+
+	std::streambuf* originalCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+	redheadDuck.Dance();
+
+	std::cout.rdbuf(originalCoutBuffer);
+
+	REQUIRE(output.str() == DANCE_MINUET);
+}
+
+TEST_CASE("Test dance method of rubber duck")
+{
+	RubberDuck rubberDuck;
+	std::ostringstream output;
+
+	std::streambuf* originalCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+	rubberDuck.Dance();
+
+	std::cout.rdbuf(originalCoutBuffer);
+
+	REQUIRE(output.str().empty());
+}
+
+TEST_CASE("Test dance method of model duck with setting dance")
+{
+	ModelDuck modelDuck;
+	std::ostringstream output;
+
+	WHEN("Model duck is not dance")
+	{
+		std::streambuf* originalCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+		modelDuck.Dance();
+
+		std::cout.rdbuf(originalCoutBuffer);
+
+		REQUIRE(output.str().empty());
+
+		modelDuck.SetDanceBehavior(std::make_unique<DanceWaltz>());
+		THEN("Model duck is dance waltz")
+		{
+			std::streambuf* originalCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+			modelDuck.Dance();
+
+			std::cout.rdbuf(originalCoutBuffer);
+
+			REQUIRE(output.str() == DANCE_WALTZ);
+		}
+	}
+
+}
---
+++ lab1/SimUDuck/ModelDuck.h
@@ -0,0 +1,25 @@
+#ifndef MODELDUCK_H
+#define MODELDUCK_H
+
+#include "Duck.h"
+#include "FlyNoWay.h"
+#include "QuackBehavior.h"
+#include "DanceNoWay.h"
+
+class ModelDuck : public Duck
+{
+public:
+	ModelDuck()
+		: Duck(std::make_unique<FlyNoWay>(),
+			std::make_unique<QuackBehavior>(),
+			std::make_unique<DanceNoWay>())
+	{
+	}
+
+	void Display() const override
+	{
+		std::cout << "I'm model duck" << std::endl;
+	}
+};
+
+#endif
