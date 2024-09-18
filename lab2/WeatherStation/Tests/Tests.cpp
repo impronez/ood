81100e5b4fa1cfce48f6f673f8668c404fe7641b
impronez <orzaev7312@gmail.com>
2024-09-18 08:56:58 +0300

    Добавил 2 задание

---
+++ lab2/WeatherStation/Tests/Tests.cpp
@@ -0,0 +1,35 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../../../catch/catch.hpp"
+#include "../Observer.h"
+#include "../WeatherData.h"
+
+class CTestDisplay : public IObserver<SWeatherInfo>
+{
+public:
+
+	typedef IObservable<SWeatherInfo> ObservableType;
+
+	CTestDisplay(ObservableType& observable)
+		: m_observable(observable) {}
+private:
+
+	void Update(SWeatherInfo const& data) override
+	{
+		m_observable.RemoveObserver(*this);
+
+		std::cout << "Current Temp " << data.temperature << std::endl;
+		std::cout << "Current Hum " << data.humidity << std::endl;
+		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+	ObservableType& m_observable;
+};
+
+TEST_CASE()
+{
+	CWeatherData weatherData;
+	CTestDisplay testDisplay(weatherData);
+
+	weatherData.RegisterObserver(testDisplay);
+	weatherData.SetMeasurements(0, 0, 770);
+}
---
+++ lab2/WeatherStation_tests/WeatherStation_tests.cpp
@@ -0,0 +1,7 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../../../catch/catch.hpp"
+
+TEST_CASE()
+{
+
+}
